0e3c764eea49e7173039e3483fd5f0a5ed566ec6
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-11 11:38:21 -0500

    Added A Second Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CounterService } from './services/counter.service';
+
 
 import { AppComponent } from './app.component';
 import { MyCounterComponent } from './my-counter/my-counter.component';
+import { MySecondCounterComponent } from './my-second-counter/my-second-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCounterComponent
+    MyCounterComponent,
+    MySecondCounterComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
